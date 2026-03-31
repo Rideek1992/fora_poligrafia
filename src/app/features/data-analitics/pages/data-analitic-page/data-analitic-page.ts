0f4bb15360dd0c34da8773dc2ad58cbe1f444610
Damian Rabiński <damian.rabinski@fora-poligrafia.pl>
2026-03-31 11:12:11 +0200

    finish data-analitic page, and start drone-service page - i have CMS data upload and now start building new view for some elements

--- src/app/features/data-analitics/pages/data-analitic-page/data-analitic-page.ts
+++ src/app/features/data-analitics/pages/data-analitic-page/data-analitic-page.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiBackendCMSService } from '../../../../core/backendCMS/apiCMS/apiBackendCMS-services';
+import { ServicePageInterface } from '../../../../core/models/service-page-interface';
+import { LoaderPage } from '../../../../shared/components/loader-page/loader-page';
+import { HeroPage } from '../../../../shared/components/hero-page/hero-page';
+import { BuissnesPage } from '../../../../shared/components/buissnes-page/buissnes-page';
+import { ScopePage } from '../../../../shared/components/scope-page/scope-page';
+import { BenefitsPage } from '../../../../shared/components/benefits-page/benefits-page';
+import { MiniHeroPage } from '../../../../shared/components/mini-hero-page/mini-hero-page';
+import { TargetPage } from '../../../../shared/components/target-page/target-page';
+import { CtaPage } from '../../../../shared/components/cta-page/cta-page';
 
 @Component({
   selector: 'app-data-analitic-page',
-  imports: [],
+  imports: [
+    LoaderPage,
+    HeroPage,
+    BuissnesPage,
+    ScopePage,
+    BenefitsPage,
+    MiniHeroPage,
+    TargetPage,
+    CtaPage,
+  ],
   templateUrl: './data-analitic-page.html',
   styleUrl: './data-analitic-page.scss',
 })
-export class DataAnaliticPage {}
+export class DataAnaliticPage implements OnInit {
+  dataAnaliticData: ServicePageInterface | null = null;
+  isActive: boolean = false;
+
+  constructor(private api: ApiBackendCMSService) {}
+
+  ngOnInit(): void {
+    this.api.getPageData('analiza-danych').subscribe({
+      next: (data) => {
+        this.dataAnaliticData = data;
+        this.isActive = true;
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+        this.isActive = false;
+      },
+    });
+  }
+}
--- src/app/features/dorne-services/pages/drone-service-page/drone-service-page.ts
+++ src/app/features/dorne-services/pages/drone-service-page/drone-service-page.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiBackendCMSService } from '../../../../core/backendCMS/apiCMS/apiBackendCMS-services';
+import { ServicePageInterface } from '../../../../core/models/service-page-interface';
+import { LoaderPage } from '../../../../shared/components/loader-page/loader-page';
+import { HeroPage } from '../../../../shared/components/hero-page/hero-page';
+import { BuissnesPage } from '../../../../shared/components/buissnes-page/buissnes-page';
+import { ScopePage } from '../../../../shared/components/scope-page/scope-page';
+import { BenefitsPage } from '../../../../shared/components/benefits-page/benefits-page';
+import { TargetPage } from '../../../../shared/components/target-page/target-page';
+import { MiniHeroPage } from '../../../../shared/components/mini-hero-page/mini-hero-page';
+import { CtaPage } from '../../../../shared/components/cta-page/cta-page';
 
 @Component({
   selector: 'app-drone-service-page',
-  imports: [],
+  imports: [
+    LoaderPage,
+    HeroPage,
+    BuissnesPage,
+    ScopePage,
+    BenefitsPage,
+    TargetPage,
+    MiniHeroPage,
+    CtaPage,
+  ],
   templateUrl: './drone-service-page.html',
   styleUrl: './drone-service-page.scss',
 })
-export class DroneServicePage {}
+export class DroneServicePage implements OnInit {
+  dataDronService: ServicePageInterface | null | undefined = null;
+  isActive: boolean = false;
+  constructor(private api: ApiBackendCMSService) {}
+
+  ngOnInit(): void {
+    this.api.getPageData('uslugi-dronowe').subscribe({
+      next: (data) => {
+        this.dataDronService = data;
+        this.isActive = true;
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+        this.isActive = false;
+      },
+    });
+  }
+}
