d8250000b89c38239a66eff660597e06c553bbaf
Damian Rabiński <damian.rabinski@fora-poligrafia.pl>
2026-04-16 08:06:30 +0200

    change position cookies-banner in outside funciton

--- src/app/core/backendCMS/apiCMS/apiBackendCMS-services.ts
+++ src/app/core/backendCMS/apiCMS/apiBackendCMS-services.ts
@@ -42,84 +42,11 @@ export class ApiBackendCMSService {
       .get<ServicePageResponseInterface>(url)
       .pipe(map((response) => response.data[0] ?? null));
   }
-  //
-  // getMainData(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}`;
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
-  //
-  // getHeroPages(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}&populate[heroSection][populate][primaryButton]=*&populate[heroSection][populate][secondaryButton]=*&populate[heroSection][populate][imageSection][populate]=*`;
-  //
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
-  //
-  // getBusinnesPages(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}&populate[businessSection][populate][items][populate]=*`;
-  //
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
-  //
-  // getScopePage(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}&populate[scopeSection][populate][items][populate]=*`;
-  //
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
-  //
-  // getBenefitsSection(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}&populate[benefitsSection][populate]=*`;
-  //
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
-  //
-  // getTargetSection(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}&populate[targetSection][populate][items]image[populate]=*`;
-  //
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
-  //
-  // getCTASection(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}&populate[ctaSection][populate]=*`;
-  //
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
-  //
-  // getMiniHeroSection(slug: string): Observable<ServicePageInterface | null> {
-  //   const url = `${this.apiUrl}/api/fora-pg-cmses?filters[slug][$eq]=${slug}&populate[miniHeroSection][populate]=*`;
-  //
-  //   return this.http
-  //     .get<ServicePageResponseInterface>(url)
-  //     .pipe(map((response) => (response.data.length ? response.data[0] : null)));
-  // }
 
   // agregacja funkcji do pobrania danych
 
   getPageData(slug: string): Observable<any> {
     return this.getFullPageData(slug);
-    // return forkJoin({
-    //   mainData: this.getMainData(slug),
-    //   heroSection: this.getHeroPages(slug).pipe(map((data) => data?.heroSection)),
-    //   businessSection: this.getBusinnesPages(slug).pipe(map((data) => data?.businessSection)),
-    //   scopeSection: this.getScopePage(slug).pipe(map((data) => data?.scopeSection)),
-    //   benefitsSection: this.getBenefitsSection(slug).pipe(map((data) => data?.benefitsSection)),
-    //   targetSection: this.getTargetSection(slug).pipe(map((data) => data?.targetSection)),
-    //   ctaSection: this.getCTASection(slug).pipe(map((data) => data?.ctaSection)),
-    //   miniHeroSection: this.getMiniHeroSection(slug).pipe(map((data) => data?.miniHeroSection)),
-    // });
   }
 
   getPoliticPrivacy(): Observable<PoliticPrivateInterface | null> {
